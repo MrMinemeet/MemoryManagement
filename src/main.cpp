ede516acb999841d6a3b92705eb82f2b4a5d5b98
Alex | MrMinemeet <alex@wtf-my-code.works>
2023-10-19 13:59:33 +0200

    Initial structure for Task 01
    Co-authored-by: BioGustav | Tobias <BioGustav@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,41 @@
 #include <iostream>
+#include <string>
+
+class Heap {
+	private:
+		const int HEAP_SIZE = 32 * 1024; // 32 KB
+		void* heap_buffer = malloc(HEAP_SIZE);
+
+
+	public:
+		// Constructor
+		Heap() {
+			std::cout << "A new heap has been created!" << std::endl;
+		}
+
+		// Destructor
+		~Heap() {
+			free(heap_buffer);
+			std::cout << "A heap has been destroyed!" << std::endl;
+		}
+
+		// Methods
+		void* alloc(int size) {
+			std::cout << "Allocating " << size << " bytes of memory..." << std::endl;
+		}
+
+		void dealloc() {
+			std::cout << "Deallocating memory..." << std::endl;
+			
+		}
+};
 
 int main() {
 	std::cout << "Hello, World!" << std::endl;
+
+	Heap heap;
+	heap.alloc(10);
+	heap.dealloc();
+
 	return 0;
 }
