3cb340f56c27b19636b2099e6a699ee91e12e081
MrMinemeet <alex@wtf-my-code.works>
2023-12-11 11:20:10 +0100

    Give objectSize field proper name

--- src/TypeDescriptor.cpp
+++ src/TypeDescriptor.cpp
@@ -3,7 +3,7 @@
 // Define member functions for the TypeDescriptor class here, if needed
 TypeDescriptor::TypeDescriptor(int obj_size, int* pointer_offset_array, int offset_amount) {
     // Constructor implementation
-    this->objectOffset = obj_size;
+    this->objectSize = obj_size;
 	this->offsetAmount = offset_amount;
 	this->pointerOffsetArray = pointer_offset_array;
 }
@@ -15,7 +15,7 @@ TypeDescriptor::~TypeDescriptor() {
 
 std::string TypeDescriptor::ToString() {
 	// ToString implementation
-	std::string str = "Object size: " + std::to_string(this->objectOffset) + "\n";
+	std::string str = "Object size: " + std::to_string(this->objectSize) + "\n";
 	str += "Offset amount: " + std::to_string(this->offsetAmount) + "\n";
 	str += "Pointer offset array: [";
 	for (int i = 0; i < this->offsetAmount; i++) {
