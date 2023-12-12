a7b3eec8e61aeb772fa908d536930dce61042ed7
Alex | MrMinemeet <alex@wtf-my-code.works>
2023-12-12 11:21:42 +0100

    Merge Heap::alloc(size) into Heap::alloc(TypeDescr)

--- src/Heap.hpp
+++ src/Heap.hpp
@@ -1,37 +1,36 @@
 #ifndef HEAP_HPP
 #define HEAP_HPP
 
-#include <unordered_map>
-#include <string>
-#include "Declarations.hpp"
 #include "Block.hpp"
+#include "Declarations.hpp"
+#include <string>
+#include <unordered_map>
 
+#include "TypeDescriptor.hpp"
 #include <iostream>
 #include <list>
-#include "TypeDescriptor.hpp"
 
 class Heap {
 private:
 	// TODO: Adjust heap to 32 KiB for the final submission
-    const uint HEAP_SIZE = 8 KiB;
-    void* heap_buffer;
+	const uint HEAP_SIZE = 8 KiB;
+	void* heap_buffer;
 	uint free_bytes;
-    std::list<Block*> free;
+	std::list<Block*> free;
 	std::unordered_map<std::string, TypeDescriptor*> type_map;
 
 public:
-    // Constructor
-    Heap();
+	// Constructor
+	Heap();
 
-    // Destructor
-    ~Heap();
+	// Destructor
+	~Heap();
 
-    // Methods
-	Block* alloc(int size);
+	// Methods
 	Block* alloc(const std::string& type);
-    void dealloc(Block* block);
-    bool registerType(const std::string& type, TypeDescriptor& descriptor);
+	void dealloc(Block* block);
+	bool registerType(const std::string& type, TypeDescriptor& descriptor);
 	std::string ToString();
 };
 
-#endif // HEAP_HPP
+#endif// HEAP_HPP
