ae848ab1c35d57a10d8f296ec7271b7cce856a8f
MrMinemeet <alex@wtf-my-code.works>
2023-12-20 14:47:50 +0100

    Add some todo/fixme to Declarations

--- src/Declarations.hpp
+++ src/Declarations.hpp
@@ -4,6 +4,8 @@
 #define KiB * 1024
 #define MiB * (1024 * 1024)
 
+// TODO: Change this to false for the final submission
+// FIXME: When "false", the program crashes as it can't find a typeDescriptor for "SlideBlock"
 #define DEBUG true
 
 #endif
